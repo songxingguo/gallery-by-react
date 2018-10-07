a0002755c1991060dc2d62efd4c9dd1811c14bea
songxingguo <1328989942@qqc.com>
2018-10-08 01:48:58 +0800

    添加Bootstrap

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,15 +2,20 @@ require('normalize.css/normalize.css');
 require('styles/App.css');
 
 import React from 'react';
-
-let yeomanImage = require('../images/yeoman.png');
+import  yeoman from '../../src/images/yeoman.png'
 
 class AppComponent extends React.Component {
   render() {
+    const fontStyle = {
+      width: '18rem'
+    };
     return (
-      <div className="index">
-        <img src={yeomanImage} alt="Yeoman Generator" />
-        <div className="notice">Please edit <code>src/components/Main.js</code> to get started!</div>
+      <div className="card" style={fontStyle}>
+        <img className="card-img-top" src={yeoman} alt="Card image cap" />
+          <div className="card-body">
+            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the
+              card's content.</p>
+          </div>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import 'core-js/fn/object/assign';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/Main';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap-reboot.min.css';
+import 'bootstrap/dist/css/bootstrap-grid.min.css';
 
 // Render the main component into the dom
 ReactDOM.render(<App />, document.getElementById('app'));
